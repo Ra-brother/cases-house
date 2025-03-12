0153d85b1675091a02d92e949b11ef0c5f27ec96
RICARDO ALEXANDRE IRMAO <ricardo.irmao@demprel.local>
2025-03-12 16:07:31 -0300

    Corrigindo PWA e ajustes gerais

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,11 @@ const ASSETS = [
     '/index.html',
     '/style.css',
     '/script.js',
-    '/imagens/produto1.jpg',
-    '/imagens/produto2.jpg',
-    '/imagens/produto3.jpg'
+    '/imagens/capahouse.webp',
+    '/imagens/carrinho-icon.png',
+    '/imagens/produto1.webp',
+    '/imagens/produto2.webp',
+    '/imagens/produto3.webp'
 ];
 
 self.addEventListener('install', (event) => {
@@ -17,6 +19,27 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).then((fetchResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
 });
